082551cc0c1244f94a7a85db9178fbcb3b9a0ab4
teimurjan <teymurgg321@gmail.com>
2017-12-17 20:53:27 +0600

    set db

--- db.js
+++ db.js
@@ -5,7 +5,12 @@ function initSequelize() {
   if (!c.has('db')) {
     throw new Error('No Database config!')
   }
-  return new Sequelize(c.get('db'));
+  return new Sequelize(process.env.DATABASE_URL, {
+    logging: false,
+    dialectOptions: {
+      ssl: true /* for SSL config since Heroku gives you this out of the box */
+    }
+  });
 }
 const sequelize = initSequelize();
 export default sequelize;
