b458a273a190329d542996c9f67202516ec0ceb9
teimurjan <teymurgg321@gmail.com>
2018-01-25 11:53:01 +0600

    Added date column migration

--- migrations/sqls/20171209145009-category-payment-up.sql
+++ migrations/sqls/20171209145009-category-payment-up.sql
@@ -13,7 +13,6 @@ CREATE TABLE "payments" (
         CONSTRAINT "payments_pkey"
         PRIMARY KEY,
     "amount" INTEGER NOT NULL,
-    "date" TIMESTAMP WITH TIME ZONE DEFAULT NOW(),    
     "categoryId" UUID
         CONSTRAINT payments_categories_fkey
         REFERENCES "categories",
---
+++ migrations/sqls/20180125055017-add-payment-date-up.sql
@@ -0,0 +1,3 @@
+/* Replace with your SQL commands */
+ALTER TABLE "payments"
+ADD "date" TIMESTAMP WITH TIME ZONE DEFAULT NOW()
